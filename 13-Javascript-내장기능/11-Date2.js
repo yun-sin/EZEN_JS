67c52eb0c6fd826ba7746023eeeb1db6f622ebc5
JYS <jyuns0710@gmail.com>
2022-09-03 20:15:04 +0900

    .

--- 13-Javascript-내장기능/11-Date2.js
+++ 13-Javascript-내장기능/11-Date2.js
@@ -1,30 +1,30 @@
-// /** 날짜계산 */
+/** 날짜계산 */
 
-// // timestamp 확인하기
-// // timestamp -> 1970년 1월 1일 자정부터 현재까지 흐른 시간을 초단위로 환산한 값
-// // getTime()함수는 timestamp를 밀리세컨드단위로 환산하여 반환한다.
-// const date = new Date();
-// const ts1 = date.getTime();
-// console.log(ts1);
+// timestamp 확인하기
+// timestamp -> 1970년 1월 1일 자정부터 현재까지 흐른 시간을 초단위로 환산한 값
+// getTime()함수는 timestamp를 밀리세컨드단위로 환산하여 반환한다.
+const date = new Date();
+const ts1 = date.getTime();
+console.log(ts1);
 
-// // 몇일이 지났는지 계산하기
-// const prevDate = new Date(date.getFullYear(), 0, 1);
-// const ts2 = prevDate.getTime();
-// const tmp1 = ts1 - ts2;
-// console.log(tmp1);
-// // 계산 결과를 원하는 단위로 환산 --> 24시간 * 60분 * 60초 * 1000
-// // 과거의 시점으로부터 지나온 시간을 계산할 경우 소수점을 내린다.
-// // 1.8일 지났습니다 -> 1일 지났습니다.
-// const day1 = Math.floor(tmp1 / (24 * 60 * 60 * 1000));
-// console.log(day1);
+// 몇일이 지났는지 계산하기
+const prevDate = new Date(date.getFullYear(), 0, 1);
+const ts2 = prevDate.getTime();
+const tmp1 = ts1 - ts2;
+console.log(tmp1);
+// 계산 결과를 원하는 단위로 환산 --> 24시간 * 60분 * 60초 * 1000
+// 과거의 시점으로부터 지나온 시간을 계산할 경우 소수점을 내린다.
+// 1.8일 지났습니다 -> 1일 지났습니다.
+const day1 = Math.floor(tmp1 / (24 * 60 * 60 * 1000));
+console.log(day1);
 
-// // 몇일이 남았는지 계산하기
-// const nextDay = new Date(date.getFullYear(), 11, 31);
-// const ts3 = nextDay.getTime();
-// const tmp2 = ts3 - ts1;
-// // 미래의 시점으로 남은 시간을 계산할 경우 소수점을 올린다.
-// //  0.4일 남았습니다 -> 1일남았습니다.
-// console.log(Math.ceil(tmp2 / (24 * 60 * 60 * 1000)));
+// 몇일이 남았는지 계산하기
+const nextDay = new Date(date.getFullYear(), 11, 31);
+const ts3 = nextDay.getTime();
+const tmp2 = ts3 - ts1;
+// 미래의 시점으로 남은 시간을 계산할 경우 소수점을 올린다.
+//  0.4일 남았습니다 -> 1일남았습니다.
+console.log(Math.ceil(tmp2 / (24 * 60 * 60 * 1000)));
 
 /** 응용 달력 출력하기*/
 // 일단 이달의 1일의 요일을 구한다.
