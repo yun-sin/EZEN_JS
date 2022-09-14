97251c97fe80a8e0e30a25fe093c4db99d07a037
jangys <jyuns0710@gmail.com>
2022-09-14 12:45:11 +0900

    HTML 생성하기

---
+++ 21-모듈-사용하기/es6/MyModule3.js
@@ -0,0 +1,18 @@
+/** Class를 모듈화 하기 */
+class MyClass {
+  #age;
+  #name;
+
+  constructor() {
+    console.log("---- MyClass의 객체가 생성되었습니다. ----");
+    this.age = 20;
+    this.name = "노드";
+  }
+
+  say() {
+    console.log("이름: " + this.name);
+    console.log("나이: " + this.age);
+  }
+}
+
+export default MyClass;
