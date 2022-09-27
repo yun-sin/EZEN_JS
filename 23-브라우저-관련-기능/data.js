ce90af713e82bba145ba6729ef6d52a6f5629652
jangys <jyuns0710@gmail.com>
2022-09-27 18:27:28 +0900

    브라우저 관련 기능 2

---
+++ 23-브라우저-관련-기능/data.js
@@ -0,0 +1,66 @@
+const webtoon = {
+    mon: [
+        {
+            title: "참교육",
+            author: "채용택/한가람",
+            point: 9.86,
+            thumbnail: "mon1.jpg"
+        },
+        {
+            title: "뷰티풀 군바리",
+            author: "설이/윤성원",
+            point: 9.81,
+            thumbnail: "mon2.jpg"
+        },
+        {
+            title: "퀘스트지상주의",
+            author: "박태준 만화회사",
+            point: 9.79,
+            thumbnail: "mon3.jpg"
+        }
+    ],
+
+    tue: [
+        {
+            title: "김부장",
+            author: "박태준 만화회사/정종택",
+            point: 9.19,
+            thumbnail: "tue1.jpg"
+        },
+        {
+            title: "여신강림",
+            author: "야옹이",
+            point: 9.38,
+            thumbnail: "tue2.jpg"
+        },
+        {
+            title: "대학원 탈출일지",
+            author: "요다",
+            point: 9.98,
+            thumbnail: "tue3.jpg"
+        }
+    ],
+
+    wed: [
+        {
+            title: "내 남편과 결혼해줘",
+            author: "LICO / 성소작",
+            point: 9.95,
+            thumbnail: "wed1.jpg"
+        },
+        {
+            title: "전지적 독자 시점",
+            author: "UMI / 슬리피-C",
+            point: 9.95,
+            thumbnail: "wed2.jpg"
+        },
+        {
+            title: "조조코믹스",
+            author: "이동건",
+            point: 9.97,
+            thumbnail: "wed3.jpg"
+        }
+    ]
+};
+
+export default webtoon;
